6bb3b21925a67b0574416c9fadc015db3bdeaaeb
RevoIDE <vanishrp.fr@gmail.com>
2022-07-04 12:55:09 +0200

    1

---
+++ src/me/revo/install/Main.java
@@ -0,0 +1,16 @@
+package me.revo.install;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame fenetre = new JFrame();
+		fenetre.setVisible(true);
+		fenetre.setSize(500, 500);
+		fenetre.setTitle("fentre tuto");
+		fenetre.setLocationRelativeTo(null);
+		fenetre.setResizable(false);
+	}
+
+}
